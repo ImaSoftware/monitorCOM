8fdc101c4dac14381649964f289577edb8718a46
daversoft <daversoft@outlook.com>
2018-09-06 17:43:56 -0500

    Creacion del programa. Falta Ventana de Configuracion,  el loop con Hilos  y la escritura del archivo

---
+++ monitorCOM/monitorCOM/COMVIEWER.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace monitorCOM
+{
+    partial class COMVIEWER
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(COMVIEWER));
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvPuertos = new System.Windows.Forms.DataGridView();
+            this.imgAuto = new System.Windows.Forms.ImageList(this.components);
+            this.btnConfPort = new System.Windows.Forms.Button();
+            this.imgBtn = new System.Windows.Forms.ImageList(this.components);
+            this.btnReadPort = new System.Windows.Forms.Button();
+            this.btnClosePort = new System.Windows.Forms.Button();
+            this.btnOpenPort = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.b_pause = new System.Windows.Forms.Button();
+            this.b_stop = new System.Windows.Forms.Button();
+            this.b_play = new System.Windows.Forms.Button();
+            this.maintimer = new System.Windows.Forms.Timer(this.components);
+            this.col_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_LW = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col_MSJ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPuertos)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lectura de Puertos Seriales ";
+            // 
+            // dgvPuertos
+            // 
+            this.dgvPuertos.AllowUserToAddRows = false;
+            this.dgvPuertos.AllowUserToDeleteRows = false;
+            this.dgvPuertos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPuertos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col_ID,
+            this.col_Nombre,
+            this.col_Status,
+            this.col_LW,
+            this.col_MSJ});
+            this.dgvPuertos.Location = new System.Drawing.Point(15, 63);
+            this.dgvPuertos.Name = "dgvPuertos";
+            this.dgvPuertos.ReadOnly = true;
+            this.dgvPuertos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPuertos.Size = new System.Drawing.Size(895, 324);
+            this.dgvPuertos.TabIndex = 2;
+            // 
+            // imgAuto
+            // 
+            this.imgAuto.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgAuto.ImageStream")));
+            this.imgAuto.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgAuto.Images.SetKeyName(0, "s_pause.png");
+            this.imgAuto.Images.SetKeyName(1, "s_play.png");
+            this.imgAuto.Images.SetKeyName(2, "s_stop.png");
+            // 
+            // btnConfPort
+            // 
+            this.btnConfPort.ImageKey = "s_conf.png";
+            this.btnConfPort.ImageList = this.imgBtn;
+            this.btnConfPort.Location = new System.Drawing.Point(570, 399);
+            this.btnConfPort.Name = "btnConfPort";
+            this.btnConfPort.Size = new System.Drawing.Size(164, 74);
+            this.btnConfPort.TabIndex = 3;
+            this.btnConfPort.Text = "Configurar";
+            this.btnConfPort.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnConfPort.UseVisualStyleBackColor = true;
+            // 
+            // imgBtn
+            // 
+            this.imgBtn.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgBtn.ImageStream")));
+            this.imgBtn.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgBtn.Images.SetKeyName(0, "s_conf.png");
+            this.imgBtn.Images.SetKeyName(1, "s_read.png");
+            this.imgBtn.Images.SetKeyName(2, "s_close.png");
+            this.imgBtn.Images.SetKeyName(3, "s_open.png");
+            // 
+            // btnReadPort
+            // 
+            this.btnReadPort.ImageKey = "s_read.png";
+            this.btnReadPort.ImageList = this.imgBtn;
+            this.btnReadPort.Location = new System.Drawing.Point(744, 399);
+            this.btnReadPort.Name = "btnReadPort";
+            this.btnReadPort.Size = new System.Drawing.Size(164, 74);
+            this.btnReadPort.TabIndex = 4;
+            this.btnReadPort.Text = "Leer";
+            this.btnReadPort.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnReadPort.UseVisualStyleBackColor = true;
+            this.btnReadPort.Click += new System.EventHandler(this.btnReadPort_Click);
+            // 
+            // btnClosePort
+            // 
+            this.btnClosePort.ImageKey = "s_close.png";
+            this.btnClosePort.ImageList = this.imgBtn;
+            this.btnClosePort.Location = new System.Drawing.Point(396, 399);
+            this.btnClosePort.Name = "btnClosePort";
+            this.btnClosePort.Size = new System.Drawing.Size(164, 74);
+            this.btnClosePort.TabIndex = 5;
+            this.btnClosePort.Text = "Cerrar";
+            this.btnClosePort.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnClosePort.UseVisualStyleBackColor = true;
+            this.btnClosePort.Click += new System.EventHandler(this.btnClosePort_Click);
+            // 
+            // btnOpenPort
+            // 
+            this.btnOpenPort.ImageKey = "s_open.png";
+            this.btnOpenPort.ImageList = this.imgBtn;
+            this.btnOpenPort.Location = new System.Drawing.Point(222, 399);
+            this.btnOpenPort.Name = "btnOpenPort";
+            this.btnOpenPort.Size = new System.Drawing.Size(164, 74);
+            this.btnOpenPort.TabIndex = 6;
+            this.btnOpenPort.Text = "Abir";
+            this.btnOpenPort.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnOpenPort.UseVisualStyleBackColor = true;
+            this.btnOpenPort.Click += new System.EventHandler(this.btnOpenPort_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.b_pause);
+            this.groupBox1.Controls.Add(this.b_stop);
+            this.groupBox1.Controls.Add(this.b_play);
+            this.groupBox1.Location = new System.Drawing.Point(12, 393);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(193, 80);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Automático";
+            // 
+            // b_pause
+            // 
+            this.b_pause.ImageKey = "s_pause.png";
+            this.b_pause.ImageList = this.imgAuto;
+            this.b_pause.Location = new System.Drawing.Point(68, 19);
+            this.b_pause.Name = "b_pause";
+            this.b_pause.Size = new System.Drawing.Size(56, 56);
+            this.b_pause.TabIndex = 9;
+            this.b_pause.UseVisualStyleBackColor = true;
+            // 
+            // b_stop
+            // 
+            this.b_stop.ImageKey = "s_stop.png";
+            this.b_stop.ImageList = this.imgAuto;
+            this.b_stop.Location = new System.Drawing.Point(130, 19);
+            this.b_stop.Name = "b_stop";
+            this.b_stop.Size = new System.Drawing.Size(56, 56);
+            this.b_stop.TabIndex = 8;
+            this.b_stop.UseVisualStyleBackColor = true;
+            // 
+            // b_play
+            // 
+            this.b_play.ImageKey = "s_play.png";
+            this.b_play.ImageList = this.imgAuto;
+            this.b_play.Location = new System.Drawing.Point(6, 19);
+            this.b_play.Name = "b_play";
+            this.b_play.Size = new System.Drawing.Size(56, 56);
+            this.b_play.TabIndex = 8;
+            this.b_play.UseVisualStyleBackColor = true;
+            // 
+            // col_ID
+            // 
+            this.col_ID.DataPropertyName = "Id";
+            this.col_ID.HeaderText = "id";
+            this.col_ID.Name = "col_ID";
+            this.col_ID.ReadOnly = true;
+            // 
+            // col_Nombre
+            // 
+            this.col_Nombre.DataPropertyName = "Nombre";
+            this.col_Nombre.HeaderText = "Nombre";
+            this.col_Nombre.Name = "col_Nombre";
+            this.col_Nombre.ReadOnly = true;
+            // 
+            // col_Status
+            // 
+            this.col_Status.DataPropertyName = "Status";
+            this.col_Status.HeaderText = "Estatus";
+            this.col_Status.Name = "col_Status";
+            this.col_Status.ReadOnly = true;
+            // 
+            // col_LW
+            // 
+            this.col_LW.DataPropertyName = "UltimaEscritura";
+            this.col_LW.HeaderText = "UltimaLectura";
+            this.col_LW.Name = "col_LW";
+            this.col_LW.ReadOnly = true;
+            // 
+            // col_MSJ
+            // 
+            this.col_MSJ.DataPropertyName = "mensaje";
+            this.col_MSJ.HeaderText = "Mensaje";
+            this.col_MSJ.Name = "col_MSJ";
+            this.col_MSJ.ReadOnly = true;
+            // 
+            // COMVIEWER
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 510);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnOpenPort);
+            this.Controls.Add(this.btnClosePort);
+            this.Controls.Add(this.btnReadPort);
+            this.Controls.Add(this.btnConfPort);
+            this.Controls.Add(this.dgvPuertos);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "COMVIEWER";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPuertos)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvPuertos;
+        private System.Windows.Forms.ImageList imgAuto;
+        private System.Windows.Forms.Button btnConfPort;
+        private System.Windows.Forms.Button btnReadPort;
+        private System.Windows.Forms.Button btnClosePort;
+        private System.Windows.Forms.Button btnOpenPort;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button b_pause;
+        private System.Windows.Forms.Button b_stop;
+        private System.Windows.Forms.Button b_play;
+        private System.Windows.Forms.Timer maintimer;
+        private System.Windows.Forms.ImageList imgBtn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_Status;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_LW;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_MSJ;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ monitorCOM/monitorCOM/COMVIEWER.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+namespace monitorCOM
+{
+    public partial class COMVIEWER : Form
+    {
+        List<easyPort> MisPuertos;
+        DataTable puertosView;
+        class easyPort
+        {
+            public int id = -1;
+            public enum status { Error, Open, Closed }
+            public int baudrate = 24000;
+            public string portName = "";
+            public SerialPort spSoft;
+            public DateTime ultWrite;
+            public status estado;
+            public string mesaje = "";
+            public easyPort(int xid, string xName = "COM1", int xbaudRate = 9600) {
+                id = xid;
+                portName = xName;
+                spSoft = new SerialPort();
+                spSoft.ReadTimeout = 10000;
+                spSoft.PortName = xName;
+                spSoft.BaudRate = xbaudRate;
+                spSoft.DataBits = 8;
+                spSoft.StopBits = StopBits.One;
+
+            }
+            public void readPort() {
+                
+                string lectura = "";
+                if (this.estado != status.Open) {
+                    this.mesaje = "El puerto no está Abierto";
+                    return;
+                }
+                try
+                {
+                    try
+                    {
+                        lectura = spSoft.ReadLine();
+                        this.ultWrite = DateTime.Now;
+                    }
+                    catch (TimeoutException ex)
+                    {
+                        this.mesaje = ex.Message; this.estado = status.Error;
+                        throw ex;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    this.mesaje = ex.Message; this.estado = status.Error;
+                    this.closePort();
+                }
+               
+                MessageBox.Show(lectura);
+            }
+            public void openPort()
+            {
+                try
+                {
+                    this.spSoft.Open();
+                }
+                catch (Exception ex)
+                {
+                    this.mesaje = ex.Message;
+                    this.estado = easyPort.status.Error;
+                    this.closePort();
+                }
+                this.estado = easyPort.status.Open;
+            }
+            public void closePort() {
+                try
+                {
+                    this.spSoft.Close();
+                }
+                catch (Exception ex)
+                {
+                    this.mesaje = ex.Message;
+                    this.estado = easyPort.status.Error;
+                    this.closePort();
+                }
+                this.estado = easyPort.status.Closed;
+            }
+
+        }
+        public COMVIEWER()
+        {
+            InitializeComponent();
+            leeSeriales();
+        }
+        void leeSeriales() {
+            MisPuertos = new List<easyPort>();
+            string[] ps_Names = SerialPort.GetPortNames();
+            int i = 0;
+            foreach (string nm in ps_Names)
+            {
+
+                MisPuertos.Add(new easyPort(i, nm));
+                i += 1;
+            }
+            ActualizaGRID();
+        }
+        void ActualizaGRID() {
+            puertosView = new DataTable();
+            puertosView.Columns.Add("Id"); puertosView.Columns.Add("Nombre"); puertosView.Columns.Add("Status");
+            puertosView.Columns.Add("UltimaEscritura"); puertosView.Columns.Add("mensaje");
+            foreach ( easyPort sp in MisPuertos) {
+                puertosView.Rows.Add(sp.id, sp.portName, sp.estado.ToString(), sp.ultWrite.ToLongTimeString(), sp.mesaje);
+            }
+            dgvPuertos.DataSource = puertosView;
+            dgvPuertos.Update();
+        }
+
+        private void btnOpenPort_Click(object sender, EventArgs e)
+        {
+            DataRowView dvr = (DataRowView)dgvPuertos.SelectedRows[0].DataBoundItem;
+            MisPuertos[Convert.ToInt32(dvr.Row["Id"])].openPort();
+            ActualizaGRID();
+        }
+
+        private void btnClosePort_Click(object sender, EventArgs e)
+        {
+            DataRowView dvr = (DataRowView)dgvPuertos.SelectedRows[0].DataBoundItem;
+            MisPuertos[Convert.ToInt32(dvr.Row["Id"])].closePort();
+            ActualizaGRID();
+        }
+
+        private void btnReadPort_Click(object sender, EventArgs e)
+        {
+            DataRowView dvr = (DataRowView)dgvPuertos.SelectedRows[0].DataBoundItem;
+            MisPuertos[Convert.ToInt32(dvr.Row["Id"])].readPort();
+            ActualizaGRID();
+        }
+    }
+}
